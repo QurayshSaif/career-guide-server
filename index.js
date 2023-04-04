d1297efa67c8064d0e5f5bc53a60807293bc9a24
QurayshSaif <saifquraysh@gmaill.com>
2023-04-04 12:14:38 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+// const questionRoutes = require("./routes/questions.js");
+
+//environment variables
+const PORT = process.env.PORT || 5050;
+const CLIENT_URL = process.env.CLIENT_URL;
+
+// Allow requests from client url
+app.use(
+  cors({
+    origin: CLIENT_URL,
+  })
+);
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+// app.use("/questions", questionRoutes);
+
+app.listen(PORT, () => {
+  console.log("server started");
+});
